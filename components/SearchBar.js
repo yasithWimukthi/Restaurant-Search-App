072c4c55c5e0051c4e606a92a02cbd6e1200c40e
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-26 23:44:24 +0530

    Managing states

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -2,13 +2,18 @@ import React from 'react';
 import {View,TextInput,StyleSheet} from 'react-native';
 import {Feather} from '@expo/vector-icons';
 
-const SearchScreen = () =>{
+const SearchScreen = ({term,onTermChanged,onTermSubmitted}) =>{
     return (
         <View style={styles.background}>
             <Feather name="search" style={styles.iconStyles}/>
             <TextInput
+                autoCapitalize='none'
+                autoCorrect={false}
                 style={styles.inputStyle}
                 placeholder="Search"
+                value={term}
+                onChangeText={newTerm => onTermChanged(newTerm)}
+                onEndEditing={() => onTermSubmitted()}
             />
         </View>
     );
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {View,Text,StyleSheet} from 'react-native';
 import SearchBar from "../../components/SearchBar";
 
 const SearchScreen = () =>{
+
+    const[term,setTerm] = useState('');
+
     return (
       <View>
-          <SearchBar />
+          <SearchBar
+            term={term}
+            onTermChanged={ newTerm => setTerm(newTerm)}
+            onTermSubmitted={ () => console.log('submitted')}
+          />
           <Text>Search screen</Text>
       </View>
     );
