7c9baa1a8921f81ba0d5f0f7658268c82a6d8667
yasithWimukthi <it19966922@my.sliit.lk>
2021-02-26 19:30:23 +0530

    Implement stack navigator

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
-import { createStackNavigator} from 'react-navigation/stack';
+import { createStackNavigator} from 'react-navigation-stack';
 import {createAppContainer} from 'react-navigation';
 import SearchScreen from "./src/screens/SearchScreen";
 
-const navigator = createStackNavigator({
-    Search : SearchScreen
-},{
-    initialRouteName : 'Search',
-    defaultNavigationOptions : {
-        title : 'Business Search'
+    const screens = {
+        Search: {
+            screen: SearchScreen,
+            navigationOptions: {
+                title: 'Business Search',
+                //headerStyle: { backgroundColor: '#eee' }
+            }
+        }
+    };
+
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerTintColor: '#444',
+        headerStyle: { backgroundColor: '#eee', height: 60 }
     }
 });
 
-export default createAppContainer(navigator);
+export default createAppContainer(HomeStack);
