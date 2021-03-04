0f2fe9dfc9f84d7342e2e43fb1155003ebb4af3a
yasithWimukthi <it19966922@my.sliit.lk>
2021-03-04 20:56:16 +0530

    Create Results Show Screen

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
-import { createStackNavigator} from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation';
+import {createAppContainer,createStackNavigator} from 'react-navigation';
+
 import SearchScreen from "./src/screens/SearchScreen";
+import ResultsShowScreen from "./src/screens/ResultsShowScreen";
 
-    const screens = {
-        Search: {
-            screen: SearchScreen,
-            navigationOptions: {
-                title: 'Business Search',
-                //headerStyle: { backgroundColor: '#eee' }
-            }
+const navigator = createStackNavigator(
+    {
+        Search: SearchScreen,
+        ResultShow: ResultsShowScreen
+    },
+    {
+        initialRouteName: 'Search',
+        defaultNavigationOptions :{
+            title : 'Business Search'
         }
-    };
-
-const HomeStack = createStackNavigator(screens, {
-    defaultNavigationOptions: {
-        headerTintColor: '#444',
-        headerStyle: { backgroundColor: '#eee', height: 60 }
     }
-});
+);
 
-export default createAppContainer(HomeStack);
+
+export default createAppContainer(navigator);
